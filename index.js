1d73687c0200ec43a3ac83d8cb838278ed28a62a
JackHarrison225 <jh.rrison225@gmail.com>
2024-02-13 11:29:15 +0000

    functions added

--- index.js
+++ index.js
@@ -74,13 +74,13 @@ app.post("/signup", async (req, res) => {
           newUser.Password = hash
           newUser.PFP = ""
           newUser.Bio = "Bio"
+          newUser.Email = ""
           const user = new User(newUser);
           console.log("Created a user")
           await user.save();
           res.send({ message: "New User Created." });
           });
      });
-     
      
 });
 //#####################################//
@@ -98,17 +98,18 @@ app.post("/auth", async (req, res) => {
      let matchPass = await bcrypt.compare(Input, Database);
      
 
-     // do not store password in plain text
+     
      if (!matchPass) 
      {
           console.log("wrong password");
           return res.sendStatus(403);
      }
-     // code to generate token
+
      user.Token = uuidv4();
      console.log("Make token")
      await user.save();
      res.send({Token: user.Token})
+
      //create expiredate
      //user.ExpireDate = new Date
 });
@@ -133,6 +134,101 @@ app.use(async (req, res, next) => {
           return res.sendStatus(403);
      }
 });
+//#####################################//
+
+//######## User Administration ########//
+app.patch("/forgotPassword", async (req, res) => {
+     let email = req.body.email
+     let username = req.body.username
+
+     const user = await User.findOne({Username: username, Email: email})
+
+     if(user)
+     {
+          // create random password 
+          // email the new password to the given email
+     }
+     else
+     {
+          res.sendStatus(403)
+     }
+});
+
+app.patch("/changeUsername", async(req, res) => {
+     let email = req.body.email
+     let password = req.body.password
+     let username = req.body.username
+     let newUsername = req.body.newUsername
+
+     const user = await User.findOne({Username: username, Email: email, Password: password})
+
+     user.username = newUsername
+     await user.save()
+
+     const projects = user.Projects
+     
+     for (let i = 0; i < projects.length; i ++)
+     {
+          CurrentProjectId = projects[i]
+          CurrentProject = await Project.findOne({_id: CurrentProjectId})
+          for(let x = 0; x < CurrentProject.Team; x++)
+          {
+               if(CurrentProject.Team[x] == username)
+               {
+                    CurrentProject.Team[x] = newUsername
+                    break
+               }
+               else if( x < CurrentProject.Owners.length)
+               {
+                    if(CurrentProject.Owners[x] == username)
+                    {
+                         CurrentProject.Owners[x] = newUsername
+                         break
+                    }
+               }
+          } 
+          await CurrentProject.save()
+     }
+})
+
+app.patch("/changePassword", async(req, res) => {
+     // patch
+     // if from link take userID
+     // find user with userID and change password to new password
+     // if from profile take username and email and password to change password
+     // find user with username, email check oldpassword and change to new password
+          
+})
+
+app.patch("/addFriend", async(req, res) => {
+     let username = req.body.username
+     let friend = req.body.user
+     let token = req.body.token
+
+     let user = await User.findOne({Username: username, Token: token})
+
+     if(user)
+     {
+          user.Friends = [...user.Friends, friend]
+     }
+     else
+     {
+          res.send(403)
+     }
+
+})
+
+app.patch("/addTeam", async(req, res) => {
+     // patch
+     // check username in project owner
+     // add user to projects team
+})
+
+app.patch("/addOwner", async(req, res) => {
+     // patch
+     // check username in project owner
+     // add user to projects owners and remove from team    
+})
 //#####################################//
 
 //###### Project Administration #######//
